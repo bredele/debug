5bacc68f7cbd0491b6c8c9f730bdee51a747c16e
bredele <olivier.wietrich@gmail.com>
2014-05-08 00:40:15 -0600

    initialize component

---
+++ index.js
@@ -0,0 +1,59 @@
+
+/**
+ * Module dependencies.
+ * @api private
+ */
+
+var trace = require('trace');
+
+
+/**
+ * Helpers.
+ */
+
+var s = 1000;
+var m = s * 60;
+var h = m * 60;
+
+
+/**
+ * Expose 'debug'
+ */
+
+module.exports = debug;
+
+
+/**
+ * debug constructor.
+ * @api public
+ */
+
+function debug() {
+  trace.on('log', function(name, ms, fmt) {
+  	fmt = name
+  	  + ' '
+  	  + fmt
+  	  + ' +' + debug.humanize(ms);
+
+  	window.console
+  	  && console.log
+  	  && Function.prototype.apply.call(console.log, console, arguments);
+
+  });
+}
+
+
+/**
+ * Humanized format for `ms`.
+ *
+ * @param {Number} ms
+ * @return {String}
+ * @api private
+ */
+
+debug.humanize = function(ms) {
+  if (ms >= h) return Math.round(ms / h) + 'h';
+  if (ms >= m) return Math.round(ms / m) + 'm';
+  if (ms >= s) return Math.round(ms / s) + 's';
+  return ms + 'ms';
+}
